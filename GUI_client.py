2ad65a0406b76903539ed292c9f4331b204b5dba
Radu <raduolaret1@gmail.com>
2022-12-06 13:00:32 +0200

    Refactored client script into OOP and implemented within GUI

--- GUI_client.py
+++ GUI_client.py
@@ -1,17 +1,23 @@
 import tkinter as tk
 from tkinter import *
 
-class GUI_client():
+import client
 
-    def __init__(self,gui_client):
+
+class GuiClient:
+
+    def __init__(self, gui_client):
         super().__init__()
-        #initializare fereastra
+        # initializare backend
+        self.backend = client.Client(self)
+
+        # initializare fereastra
         gui_client.title("DHCP Client")
         gui_client.geometry("900x600")
-        gui_client.resizable(False,False)
+        gui_client.resizable(False, False)
 
         self.opt_frame = Frame(gui_client)
-        self.right_frame=Frame(gui_client)
+        self.right_frame = Frame(gui_client)
 
         # initializam variabilele pentru checkbox uri
         self.SUBNET_MASK = IntVar()
@@ -27,38 +33,40 @@ class GUI_client():
         self.END = IntVar()
 
         # cand o sa fie gata clasa client,aceast buton va avea command=self.discover
-        self.button_start=Button(self.opt_frame,height=2,width=10,text="Start")
-        self.ip_entry=Entry(self.opt_frame)
-        self.lease_entry=Entry(self.opt_frame)
+        self.button_start = Button(self.opt_frame, command=self.backend.send_discover(), height=2, width=10,
+                                   text="Start")
+        self.ip_entry = Entry(self.opt_frame)
+        self.lease_entry = Entry(self.opt_frame)
 
-        self.text=Text(self.right_frame,width=45,height=16)
-        self.text_terminal=Text(self.right_frame,width=45,height=16)
+        self.text = Text(self.right_frame, state='disabled', width=45, height=16)
+        self.text_terminal = Text(self.right_frame, state='disabled', width=45, height=16)
 
     def disable_entry(self):
-        if(self.REQUESTED_IP_ADDRESS.get()==1):
+        if self.REQUESTED_IP_ADDRESS.get() == 1:
             self.ip_entry.config(state=NORMAL)
-        elif(self.REQUESTED_IP_ADDRESS.get()==0):
+        elif self.REQUESTED_IP_ADDRESS.get() == 0:
             self.ip_entry.config(state=DISABLED)
+
     def disable_entry_lease(self):
-        if(self.LEASE_TIME.get()==1):
+        if self.LEASE_TIME.get() == 1:
             self.lease_entry.config(state=NORMAL)
-        elif(self.LEASE_TIME.get()==0):
+        elif self.LEASE_TIME.get() == 0:
             self.lease_entry.config(state=DISABLED)
 
     def run(self):
         self.opt_frame.grid(row=0, column=0)
-        self.right_frame.grid(row=0,column=1,padx=60)
+        self.right_frame.grid(row=0, column=1, padx=60)
 
-        opt_label=Label(self.opt_frame,text="DHCP Options")
+        opt_label = Label(self.opt_frame, text="DHCP Options")
         opt_label.grid(row=1, column=1, padx=50, pady=20)
 
-        par_label=Label(self.right_frame,text="Current Parameters")
-        par_label.grid(row=0,column=0)
+        par_label = Label(self.right_frame, text="Current Parameters")
+        par_label.grid(row=0, column=0)
         self.text.grid(row=1, column=0)
 
-        terminal_label=Label(self.right_frame,text="Operations")
-        terminal_label.grid(row=2,column=0)
-        self.text_terminal.grid(row=3,column=0)
+        terminal_label = Label(self.right_frame, text="Operations")
+        terminal_label.grid(row=2, column=0)
+        self.text_terminal.grid(row=3, column=0)
 
         opt1_label = Label(self.opt_frame, text="Option 1: Subnet Mask")
         opt2_label = Label(self.opt_frame, text="Option 3: Router")
@@ -75,8 +83,10 @@ class GUI_client():
         opt1_ck = Checkbutton(self.opt_frame, variable=self.SUBNET_MASK, height=2, width=4)
         opt2_ck = Checkbutton(self.opt_frame, variable=self.ROUTER, height=2, width=4)
         opt3_ck = Checkbutton(self.opt_frame, variable=self.DNS, height=2, width=4)
-        opt4_ck = Checkbutton(self.opt_frame, variable=self.REQUESTED_IP_ADDRESS, height=2, width=4,command=self.disable_entry)
-        opt5_ck = Checkbutton(self.opt_frame, variable=self.LEASE_TIME, height=2, width=4,command=self.disable_entry_lease)
+        opt4_ck = Checkbutton(self.opt_frame, variable=self.REQUESTED_IP_ADDRESS, height=2, width=4,
+                              command=self.disable_entry)
+        opt5_ck = Checkbutton(self.opt_frame, variable=self.LEASE_TIME, height=2, width=4,
+                              command=self.disable_entry_lease)
         opt6_ck = Checkbutton(self.opt_frame, variable=self.MESSAGE_TYPE, height=2, width=4)
         opt7_ck = Checkbutton(self.opt_frame, variable=self.SERVER_IDENTIFIER, height=2, width=4)
         opt8_ck = Checkbutton(self.opt_frame, variable=self.PARAMETER_REQUESTED_LIST, height=2, width=4)
@@ -88,11 +98,11 @@ class GUI_client():
         opt2_label.grid(row=3, column=1, sticky='w')
         opt3_label.grid(row=4, column=1, sticky='w')
         opt4_label.grid(row=5, column=1, sticky='w')
-        self.ip_entry.config(state="disable")
-        self.ip_entry.grid(row=5,column=2)
+        self.ip_entry.config(state="disabled")
+        self.ip_entry.grid(row=5, column=2)
         opt5_label.grid(row=6, column=1, sticky='w')
-        self.lease_entry.config(state="disable")
-        self.lease_entry.grid(row=6,column=2)
+        self.lease_entry.config(state="disabled")
+        self.lease_entry.grid(row=6, column=2)
         opt6_label.grid(row=7, column=1, sticky='w')
         opt7_label.grid(row=8, column=1, sticky='w')
         opt8_label.grid(row=9, column=1, sticky='w')
@@ -112,17 +122,27 @@ class GUI_client():
         opt10_ck.grid(row=11, column=0)
         opt11_ck.grid(row=12, column=0)
 
-        self.button_start.grid(row=15,column=0,padx=20,pady=20)
+        self.button_start.grid(row=15, column=0, padx=20, pady=20)
 
-    def write_text(self,information):
-        self.text.insert(END,str(information))
+    def write_text(self, information):
+        self.text.insert(END, str(information))
 
     def delete_text(self):
-        self.text.delete(0,END)
+        self.text.delete(0, END)
 
-if __name__=="__main__":
-    gui=tk.Tk()
-    gui_client=GUI_client(gui).run()
+    # cod preluat de la https://tkdocs.com/tutorial/text.html, sectiunea example: logging window
+    def write_to_terminal(self, msg):
+        numlines = int(self.text_terminal.index('end - 1 line').split('.')[0])
+        self.text_terminal['state'] = 'normal'
+        if numlines == 24:
+            self.text_terminal.delete(1.0, 2.0)
+        if self.text_terminal.index('end-1c') != '1.0':
+            self.text_terminal.insert('end', '\n')
+        self.text_terminal.insert('end', msg)
+        self.text_terminal['state'] = 'disabled'
+
+
+if __name__ == "__main__":
+    gui = tk.Tk()
+    GuiClient(gui).run()
     gui.mainloop()
-
-
--- client.py
+++ client.py
@@ -1,68 +1,51 @@
 import socket
 import sys
 import random
-
 import select
 import threading
+
 import DHCP_Message
+from GUI_client import GuiClient
 
 
 def gen_xid():
     return random.randint(1, 0xffffffff)
 
-def receive_fct():
-    global running
-    contor = 0
-    while running:
-        # Apelam la functia sistem IO -select- pentru a verifca daca socket-ul are date in bufferul de receptie
-        # Stabilim un timeout de 1 secunda
-        r, _, _ = select.select([s], [], [], 1)
-        if not r:
-            contor = contor + 1
-        else:
-            data, address = s.recvfrom(1024)
 
-            #
-            print("S-a receptionat ", str(data), " de la ", address)
-            print("Contor= ", contor)
+# o functie pentru random MAC address?
 
 
-# Citire nr port din linia de comanda
-if len(sys.argv) != 4:
-    print("help : ")
-    print("  --sport=numarul_meu_de_port ")
-    print("  --dport=numarul_de_port_al_peer-ului ")
-    print("  --dip=ip-ul_peer-ului ")
-    sys.exit()
+class Client:
 
-for arg in sys.argv:
-    if arg.startswith("--sport"):
-        temp, sport = arg.split("=")
-    elif arg.startswith("--dport"):
-        temp, dport = arg.split("=")
-    elif arg.startswith("--dip"):
-        temp, dip = arg.split("=")
+    def __init__(self, gui: GuiClient, source_port=68, destination_port=67, destination_ip='127.0.0.1'):
+        self.gui = gui
+        self.destination_port = destination_port
+        self.destination_ip = destination_ip
+        # Creare socket UDP
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        self.socket.bind(('0.0.0.0', int(source_port)))
+        self.running = True
+        try:
+            self.receive_thread = threading.Thread(target=self.receive_fct)
+            self.receive_thread.start()
+        except:
+            print("Eroare la pornirea thread‐ului")
+            sys.exit()
 
-# Creare socket UDP
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    def receive_fct(self):
+        contor = 0
+        while self.running:
+            # Apelam la functia sistem IO -select- pentru a verifca daca socket-ul are date in bufferul de receptie
+            # Stabilim un timeout de 1 secunda
+            r, _, _ = select.select([self.socket], [], [], 1)
+            if not r:
+                contor = contor + 1
+            else:
+                data, address = self.socket.recvfrom(1024)
+                #
+                self.gui.write_to_terminal("S-a receptionat " + str(data) + " de la " + address)
+                self.gui.write_to_terminal("Contor= " + contor)
 
-s.bind(('0.0.0.0', int(sport)))
-
-running = True
-
-try:
-    receive_thread = threading.Thread(target=receive_fct)
-    receive_thread.start()
-except:
-    print("Eroare la pornirea thread‐ului")
-    sys.exit()
-
-while True:
-    try:
-        data = input("Trimite: ")
-        s.sendto(DHCP_Message.getBasicDISCOVER(gen_xid(), 0xf34a93dc2116), (dip, int(dport)))
-    except KeyboardInterrupt:
-        running = False
-        print("Waiting for the thread to close...")
-        receive_thread.join()
-        break
+    def send_discover(self):
+        self.socket.sendto(DHCP_Message.getBasicDISCOVER(gen_xid(), 0xf34a93dc2116),
+                           (self.destination_ip, int(self.destination_port)))
